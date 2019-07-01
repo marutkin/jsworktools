6fa80fb8869dfc35355a7c940be7f34de416cea1
Dmitriy Marutkin <dmitriy.marutkin@firstlinesoftware.com>
2019-07-01 13:02:14 +0300

    Add dom helpers

--- src/Array/linqlike.js
+++ src/Array/linqlike.js
@@ -1,5 +1,6 @@
-﻿// Linqlike Array.prototype extensions in .Net LINQ style
-// Version: 1.0.0.0
+﻿
+// Linqlike Array.prototype extensions in .Net LINQ style
+// Version: 1.0.0.1
 
 // Is element in array
 Array.prototype.contains = function(searchValue) {
@@ -14,28 +15,37 @@ Array.prototype.where = function(predicatFunction) {
 // Get new array distincted by property name, only for array of objects
 Array.prototype.getDistinctBy = function(propertyName) {
     try {
-        var arr = this;
-        if (this.where(value => typeof value != 'object').length > 0) throw new Error('Ошибка выполнения distinctBy - в массиве есть примитивные данные.');
-        var tempStore = [];
+
+        const arr = this;
+        const tempStore = [];
+
+        if (this.where(value => typeof value != 'object').length > 0)
+        {
+          throw new Error('[ Error: distinctBy ]: Array contains primitives');
+        }
 
         function getFiltered() {
             return arr.filter(function(arrElement) {
-                if (arrElement == undefined || !(propertyName in arrElement)) return;
+                if (!arrElement || !(propertyName in arrElement)) return;
                 return !tempStore.contains(arrElement[propertyName]) && tempStore.push(arrElement[propertyName])
             })
         }
+
         return getFiltered();
+
     } catch (error) {
-        throw new Error(`Ошибка выполнения Array.prototype.getDistinctBy \n${error}`);
+        throw new Error(`[ Error: Array.prototype.getDistinctBy ]: \n${error}`);
     }
 }
 
 // Delete element from array
 Array.prototype.deleteByIndex = function(valueIndex) {
     try {
-        if (this.length <= 0 || valueIndex == undefined) throw new Error('Попытка удалить элемент в пустом массиве.');
+        if (this.length <= 0 || !valueIndex) {
+          throw new Error('Array is empty!');
+        }
         this.splice(valueIndex, 1);
     } catch (error) {
-        throw new Error(`Ошибка выполнения Array.prototype.deleteByIndex  \n${error}`);
+        throw new Error(`[ Error: Array.prototype.deleteByIndex ]: \n${error}`);
     }
 }
---
+++ src/DOM/domTools.js
@@ -0,0 +1,138 @@
+
+// Browsers DOM helper tools.
+// Version: 1.0.0.0
+
+const domReady = function (fn) {
+
+  if (typeof fn !== 'function') return;
+
+  if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    return fn();
+  }
+
+  document.addEventListener('DOMContentLoaded', fn, false);
+
+};
+
+const getElement = function (selector, parent) {
+  return (parent ? parent : document).querySelector(selector);
+};
+
+const getElements = function (selector, parent) {
+  return [].slice.call((parent ? parent : document).querySelectorAll(selector));
+};
+
+const getParents = function (elem, selector) {
+
+  let parents = [];
+
+  while (elem && elem !== document) {
+
+    if (selector) {
+      if (elem.matches(selector)) {
+        parents.push(elem);
+      }
+    } else {
+      parents.push(elem);
+    }
+
+    elem = elem.parentNode;
+
+  }
+
+  return parents;
+
+};
+
+const getSiblings = function (elem) {
+  return [].filter.call(elem.parentNode.children, (sibling) => sibling !== elem);
+};
+
+const getSibling = function (elem, selector, searchDirection) {
+
+  let sibling = elem[searchDirection];
+
+  if (!selector) return sibling;
+
+  while (sibling) {
+    if (sibling.matches(selector)) return sibling;
+    sibling = sibling[searchDirection]
+  }
+
+};
+
+const getSiblingNext = (elem, selector) => getSibling(elem, selector, 'nextElementSibling' )
+const getSiblingPrevious = (elem, selector) => getSibling(elem, selector, 'previousElementSibling' )
+
+const isOutOfViewport = function (elem) {
+
+  const bounding = elem.getBoundingClientRect();
+
+  const out = {};
+  out.top = bounding.top < 0;
+  out.left = bounding.left < 0;
+  out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+  out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+  out.any = out.top || out.left || out.bottom || out.right;
+  out.all = out.top && out.left && out.bottom && out.right;
+
+  return out;
+
+};
+
+const decodeHTML = function (html) {
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
+};
+
+const scrollDistance = function (callback, refresh) {
+
+  if (!callback || typeof callback !== 'function') return;
+
+  let isScrolling, start, end, distance;
+
+  window.addEventListener('scroll', function (event) {
+
+    if (!start) {
+      start = window.pageYOffset;
+    }
+
+    window.clearTimeout(isScrolling);
+
+    isScrolling = setTimeout(function() {
+
+      end = window.pageYOffset;
+      distance = end - start;
+
+      callback(distance, start, end);
+
+      start = null;
+      end = null;
+      distance = null;
+
+    }, refresh || 66);
+
+  }, false);
+
+};
+
+const onScrollStop = function (callback) {
+
+  if (!callback || typeof callback !== 'function') return;
+
+  let isScrolling;
+
+  window.addEventListener('scroll', function (event) {
+
+    window.clearTimeout(isScrolling);
+
+    isScrolling = setTimeout(function() {
+
+      callback();
+
+    }, 66);
+
+  }, false);
+
+};
